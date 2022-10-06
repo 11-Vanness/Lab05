ea6a4879f0fea7b82c0c7dd0ce810bef84a7d535
11-Vanness <whiteangelbeats@gmail.com>
2022-10-06 12:15:16 +0800

    Added collision to the coin and water.Added timer to the game
    when player touches the coin, the coin will be destroy and they gain 10 points for each coin they collect. If the player touches the water, they instantly loses. Also there's a timer which when time runs out they lose

---
+++ Assets/CollisionScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CollisionScript : MonoBehaviour
+{
+    public int score;
+
+    public Text ScoreTxt;
+
+   
+
+    public float timeLeft;
+    public Text TimerTxt;
+
+    private float timerValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreTxt.text = "Score : " + score;
+        TimerTxt.text = "Time: " + timeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        TimerTxt.text = "Time: " + timeLeft;
+
+        if(score == 80)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+        else if(timeLeft <= 0)
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            score += 10;
+            ScoreTxt.text = "Score : " + score;
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.tag == "Fire")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
